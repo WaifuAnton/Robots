5a32a017a7f9a5b01522556ad00b5796adf70896
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-01-25 01:39:09 +0200

    Controls and controller for Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
