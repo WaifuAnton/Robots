6892fbb5fff0a9ca81c2b198faec647e2dd4fa81
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-01-25 17:21:58 +0200

    Jumping (work with animation to be done)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    float speed = 10f;
+    float speed = 10;
+
+    [SerializeField]
+    float jump = 10;
+
     Rigidbody2D rb2d;
     Animator animator;
     bool facingRight = true;
@@ -21,6 +25,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        animator.SetBool("Grounded", rb2d.velocity.y == 0);
         if (rb2d.velocity.x > 0 && !facingRight || rb2d.velocity.x < 0 && facingRight)
             Flip();
     }
@@ -34,7 +39,17 @@ public class PlayerController : MonoBehaviour
             rb2d.AddForce(velocity, ForceMode2D.Impulse);
         }
         else
-            rb2d.velocity = Vector2.zero;
+        {
+            Vector2 velocityTemp = rb2d.velocity;
+            velocityTemp.x = 0;
+            rb2d.velocity = velocityTemp;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed && rb2d.velocity.y == 0)
+            rb2d.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
     }
 
     void Flip()
